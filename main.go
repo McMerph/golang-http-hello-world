be9a5cb2b4f1dc87d5cfcbc210e92db196e68824
Aleksandr Sidorenko <mcmerph@gmail.com>
2022-06-04 17:07:01 +0300

    Move `GetPrefix()` to `utils` package

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"http-hello-world/utils"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, GetPrefix()+"Hello, you've requested: %s\n", request.URL.Path)
+		fmt.Fprintf(writer, utils.GetPrefix()+"Hello, you've requested: %s\n", request.URL.Path)
 	})
 
 	http.ListenAndServe(":3000", nil)
