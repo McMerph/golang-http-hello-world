6b1b4d9d766a80425ef33e61a8125fe78aa31280
Aleksandr Sidorenko <mcmerph@gmail.com>
2022-06-04 17:01:27 +0300

    Add helper function in separate file

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
+		fmt.Fprintf(writer, GetPrefix()+"Hello, you've requested: %s\n", request.URL.Path)
 	})
 
 	http.ListenAndServe(":3000", nil)
---
+++ utils.go
@@ -0,0 +1,5 @@
+package main
+
+func GetPrefix() string {
+	return "prefix"
+}
